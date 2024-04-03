91e42a24b629ad089873205bd365a8edd7bf50b0
markharriscgi <mark.harris1@hmcts.net>
2024-04-03 11:42:06 +0100

    pdda-348 Add additional unit test coverage.

--- src/test/java/uk/gov/hmcts/flyway/FlywayNoOpStrategyTest.java
+++ src/test/java/uk/gov/hmcts/flyway/FlywayNoOpStrategyTest.java
@@ -13,8 +13,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 @ExtendWith(EasyMockExtension.class)
 class FlywayNoOpStrategyTest {
+    
+    private static final String TRUE = "Result is not True";
 
     @Mock
     private Flyway mockFlyway;
@@ -47,5 +52,30 @@ class FlywayNoOpStrategyTest {
         Assertions.assertThrows(PendingMigrationScriptException.class, () -> {
             classUnderTest.migrate(mockFlyway);
         });
+    }
+
+    @Test
+    void testMigrateNotAvailable() {
+        // Setup
+        MigrationInfo[] migrationInfo = {mockMigrationInfoImpl};
+        // Expects
+        EasyMock.expect(mockFlyway.info()).andReturn(mockMigrationInfoService);
+        EasyMock.expect(mockMigrationInfoService.all()).andReturn(migrationInfo);
+        EasyMock.expect(mockMigrationInfoImpl.getState()).andReturn(MigrationState.FAILED);
+        EasyMock.expect(mockMigrationInfoImpl.getScript()).andReturn("");
+        // Replay
+        EasyMock.replay(mockFlyway);
+        EasyMock.replay(mockMigrationInfoService);
+        EasyMock.replay(mockMigrationInfoImpl);
+
+        // Run
+        boolean result = false;
+        try {
+            classUnderTest.migrate(mockFlyway);
+            result = true;
+        } catch (Exception exception) {
+            fail(exception.getMessage());
+        }
+        assertTrue(result, TRUE);
     }
 }
