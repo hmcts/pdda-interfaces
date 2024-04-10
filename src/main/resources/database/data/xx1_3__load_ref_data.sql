35a71891d613082eec2c5ddbfc7d88e554b813ff
Luke Gittins <luke.gittins@hmcts.net>
2024-04-10 17:00:36 +0100

    PDDA-350 Converting XHB_ROTATION_SETS csv into sql insert

--- src/main/resources/database/data/xx1_3__load_ref_data.sql
+++ src/main/resources/database/data/xx1_3__load_ref_data.sql
@@ -1,5 +1,3 @@
 \COPY xhb_address (address_id, address_1, address_2, address_3, address_4, town, county, postcode, country, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'XHB_ADDRESS_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
 
 \COPY xhb_public_notice (public_notice_id, public_notice_desc, court_id, last_update_date, creation_date, created_by, last_updated_by, version, definitive_pn_id) FROM 'XHB_PUBLIC_NOTICE_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
-\COPY xhb_rotation_sets (rotation_set_id, court_id, description, default_yn, created_by, creation_date, last_updated_by, last_update_date, version) FROM 'XHB_ROTATION_SETS_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
