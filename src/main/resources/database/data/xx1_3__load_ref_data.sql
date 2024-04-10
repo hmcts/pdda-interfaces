ff85e42813c75760447b7ccd52c57fcc8d700393
markharriscgi <mark.harris1@hmcts.net>
2024-04-10 13:25:19 +0100

    pdda-350 Convert xhb_configured_public_notice csv into inserts.

--- src/main/resources/database/data/xx1_3__load_ref_data.sql
+++ src/main/resources/database/data/xx1_3__load_ref_data.sql
@@ -1,15 +1,5 @@
 \COPY xhb_address (address_id, address_1, address_2, address_3, address_4, town, county, postcode, country, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'XHB_ADDRESS_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
 
-\COPY xhb_ref_solicitor_firm (ref_solicitor_firm_id, solicitor_firm_name, crest_sof_id, court_id, obs_ind, short_name, dx_ref, vat_no, last_update_date, creation_date, created_by, last_updated_by, version, address_id, la_code) FROM 'XHB_REF_SOLICITOR_FIRM_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
-\COPY xhb_ref_system_code (ref_system_code_id, code, code_type, code_title, de_code, ref_code_order, last_update_date, creation_date, created_by, last_updated_by, version, court_id, obs_ind) FROM 'XHB_REF_SYSTEM_CODE_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
-\COPY xhb_sys_audit (sys_audit_id, table_to_audit, audit_table, auditable) FROM 'XHB_SYS_AUDIT_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
-\COPY xhb_sys_user_information (mercator_user_name, connection_pool_user_name) FROM 'XHB_SYS_USER_INFORMATION_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
-\COPY xhb_configured_public_notice (configured_public_notice_id, is_active, court_room_id, public_notice_id, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'XHB_CONFIGURED_PUBLIC_NOTICE_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
 \COPY xhb_court_log_event_desc (event_desc_id, flagged_event, editable, send_to_mercator, update_linked_cases, publish_to_subscribers, clear_public_displays, e_inform, public_display, linked_case_text, event_description, version, last_updated_by, event_type, created_by, creation_date, last_update_date, public_notice, short_description) FROM 'XHB_COURT_LOG_EVENT_DESC_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
 
 \COPY xhb_cr_live_display (cr_live_display_id, court_room_id, scheduled_hearing_id, time_status_set, status, created_by, creation_date, last_updated_by, last_update_date, version) FROM 'XHB_CR_LIVE_DISPLAY_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
