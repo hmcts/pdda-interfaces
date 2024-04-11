e8868f60cc3aafc8e7fa7ace86e2e5a725812685
markharriscgi <mark.harris1@hmcts.net>
2024-04-11 09:08:36 +0100

    pdda-350 Convert xhb_address csv into insert statements.

--- src/main/resources/database/data/xx1_3__load_ref_data.sql
+++
@@ -1,3 +0,0 @@
-\COPY xhb_address (address_id, address_1, address_2, address_3, address_4, town, county, postcode, country, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'XHB_ADDRESS_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
-
-\COPY xhb_public_notice (public_notice_id, public_notice_desc, court_id, last_update_date, creation_date, created_by, last_updated_by, version, definitive_pn_id) FROM 'XHB_PUBLIC_NOTICE_DATA_TABLE.csv' DELIMITER ',' CSV HEADER;
