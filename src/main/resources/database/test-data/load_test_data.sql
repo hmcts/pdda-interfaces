809f0ad9792badab907b79e01b6bf4d7fbba4cfa
Luke Gittins <luke.gittins@hmcts.net>
2024-04-18 12:22:13 +0100

    PDDA-350 Converting XHB_SCHED_HEARING_ATTENDEE csv into sql insert

--- src/main/resources/database/test-data/load_test_data.sql
+++ src/main/resources/database/test-data/load_test_data.sql
@@ -1,6 +1,3 @@
-\copy xhb_sched_hearing_attendee (sh_attendee_id, scheduled_hearing_id, ref_judge_id, attendee_type, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'xhb_sched_hearing_attendee.csv'  DELIMITER ',' CSV HEADER
-
-
 \i XHB_CASE_DATA_TABLE.sql
 \i XHB_CLOB_DATA_TABLE.sql
 \i XHB_FORMATTING_DATA_TABLE.sql
@@ -18,3 +15,4 @@
 \i XHB_SCHED_HEARING_DEFENDANT_DATA_TABLE.sql
 \i XHB_COURT_LOG_ENTRY_DATA_TABLE.sql
 \i XHB_REF_JUDGE_DATA_TABLE.sql
+\i XHB_SCHED_HEARING_ATTENDEE_DATA_TABLE.sql
