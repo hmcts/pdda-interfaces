20f519c8ec3f2ef6a31d8e894566229abdc96c09
Luke Gittins <luke.gittins@hmcts.net>
2024-04-18 11:37:58 +0100

    PDDA-350 Converting XHB_DEFENDANT_ON_CASE test data csv into sql insert

--- src/main/resources/database/test-data/load_test_data.sql
+++ src/main/resources/database/test-data/load_test_data.sql
@@ -1,4 +1,3 @@
-\copy xhb_defendant_on_case (defendant_on_case_id, defendant_id, case_id, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'xhb_defendant_on_case.csv'  DELIMITER ',' CSV HEADER
 \copy xhb_sched_hearing_defendant (sched_hear_def_id, scheduled_hearing_id, defendant_on_case_id, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'xhb_sched_hearing_defendant.csv'  DELIMITER ',' CSV HEADER
 \copy xhb_court_log_entry (entry_id, case_id, event_desc_id, defendant_on_case_id, scheduled_hearing_id, log_entry_xml, date_time, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'xhb_court_log_entry.csv'  DELIMITER ',' CSV HEADER
 \copy xhb_ref_judge (ref_judge_id, judge_type, full_list_title1, first_name, middle_name, surname, court_id, last_update_date, creation_date, created_by, last_updated_by, version) FROM 'xhb_ref_judge.csv'  DELIMITER ',' CSV HEADER
@@ -18,3 +17,4 @@
 \i XHB_SITTING_DATA_TABLE.sql
 \i XHB_SCHEDULED_HEARING_DATA_TABLE.sql
 \i XHB_DEFENDANT_DATA_TABLE.sql
+\i XHB_DEFENDANT_ON_CASE_DATA_TABLE.sql
