3c61954ba649baa7c207bcf0f1021396c8907c25
nathantoft <154344032+nathantoft@users.noreply.github.com>
2024-04-24 16:01:45 +0100

    Removing DailyListNotifier and making it internal using CourtConfigurationChange with scheduled job

--- src/main/java/uk/gov/hmcts/pdda/business/services/dailylistnotifier/DailyListNotifierControllerBean.java
+++ src/main/java/uk/gov/hmcts/pdda/business/services/dailylistnotifier/DailyListNotifierControllerBean.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.pdda.business.services.dailylistnotifier;
 
+import com.pdda.hb.jpa.EntityManagerUtil;
 import jakarta.ejb.ApplicationException;
 import jakarta.ejb.LocalBean;
 import jakarta.ejb.Stateless;
@@ -10,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.framework.scheduler.RemoteTask;
 import uk.gov.hmcts.pdda.business.AbstractControllerBean;
+import uk.gov.hmcts.pdda.business.services.pdda.PddaDlNotifierHelper;
+import uk.gov.hmcts.pdda.business.services.publicdisplay.PdConfigurationControllerBean;
 import uk.gov.hmcts.pdda.common.publicdisplay.events.ConfigurationChangeEvent;
 import uk.gov.hmcts.pdda.common.publicdisplay.jms.PublicDisplayNotifier;
 import uk.gov.hmcts.pdda.common.publicdisplay.types.configuration.CourtConfigurationChange;
@@ -27,6 +30,8 @@ public class DailyListNotifierControllerBean extends AbstractControllerBean impl
     private static final String ENTERED = " : entered";
     
     private PublicDisplayNotifier publicDisplayNotifier;
+    private PddaDlNotifierHelper pddaDlNotifierHelper;
+    private PdConfigurationControllerBean pdConfigurationController;
 
     public DailyListNotifierControllerBean(EntityManager entityManager) {
         super(entityManager);
@@ -38,23 +43,31 @@ public class DailyListNotifierControllerBean extends AbstractControllerBean impl
 
     /**
      * Implementation of RemoteTask so that this process is called by the timer process.
+     * This scheduled job replaces the need for DailyListNotifier due to the removal of
+     * ActiveMQ.
      * 
      */
     @Override
     public void doTask() {
-        LOG.debug("Entered DL Notifier doTask method");
-        callDlHelper();
+        callDailyListNotifierHelper();
     }
 
     /**
      * Implementation of RemoteTask so that this process is called by the timer process.
      * 
      */
-    public void callDlHelper() {
-        String methodName = "callDlHelper(" + METHOD_END;
+    public void callDailyListNotifierHelper() {
+        String methodName = "callDailyListNotifierHelper(" + METHOD_END;
+        int[] courtsForPublicDisplay;
         LOG.debug(methodName + ENTERED);
-        //Call PddaDlNotifierHelper here
-        //Call refreshPublicDisplaysForCourt()
+        PdConfigurationControllerBean pdConfigurationControllerBean = getPdConfigurationControllerBean();
+        // Write to the XHB_PDDA_DL_NOTIFIER table
+        getPddaDlNotifierHelper().runDailyListNotifier();
+        // Notify the public displays that it is a new day so it can accept a new daily list
+        courtsForPublicDisplay = pdConfigurationControllerBean.getCourtsForPublicDisplay();
+        for (int court : courtsForPublicDisplay) {
+            refreshPublicDisplaysForCourt(court);
+        }
     }
 
     /**
@@ -69,10 +82,39 @@ public class DailyListNotifierControllerBean extends AbstractControllerBean impl
         getPublicDisplayNotifier().sendMessage(ccEvent);
     }
 
+    /**
+     * Returns a reference to the publicDisplayNotifier object.
+     * 
+     * @return publicDisplayNotifier
+     */
     private PublicDisplayNotifier getPublicDisplayNotifier() {
         if (publicDisplayNotifier == null) {
             publicDisplayNotifier = new PublicDisplayNotifier();
         }
         return publicDisplayNotifier;
+    }
+    
+    /**
+     * Returns a reference to the pddaDlNotifierHelper object.
+     * 
+     * @return pddaDlNotifierHelper
+     */
+    private PddaDlNotifierHelper getPddaDlNotifierHelper() {
+        if (pddaDlNotifierHelper == null) {
+            pddaDlNotifierHelper = new PddaDlNotifierHelper(EntityManagerUtil.getEntityManager());
+        }
+        return pddaDlNotifierHelper;
+    }
+    
+    /**
+     * Returns a reference to the pdConfigurationController object.
+     * 
+     * @return PDConfigurationControllerBean
+     */
+    private PdConfigurationControllerBean getPdConfigurationControllerBean() {
+        if (pdConfigurationController == null) {
+            return new PdConfigurationControllerBean();
+        }
+        return pdConfigurationController;
     }
 }
--- src/main/java/uk/gov/hmcts/pdda/business/services/pdda/PddaDlNotifierHelper.java
+++ src/main/java/uk/gov/hmcts/pdda/business/services/pdda/PddaDlNotifierHelper.java
@@ -8,8 +8,6 @@ import uk.gov.hmcts.pdda.business.entities.xhbcourt.XhbCourtDao;
 import uk.gov.hmcts.pdda.business.entities.xhbcourt.XhbCourtRepository;
 import uk.gov.hmcts.pdda.business.entities.xhbpddadlnotifier.XhbPddaDlNotifierDao;
 import uk.gov.hmcts.pdda.business.entities.xhbpddadlnotifier.XhbPddaDlNotifierRepository;
-import uk.gov.hmcts.pdda.integration.publicdisplay.jms.DailyListNotifier;
-import uk.gov.hmcts.pdda.integration.publicdisplay.jms.NotifierAttributes;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -38,23 +36,15 @@ import java.util.Optional;
 public class PddaDlNotifierHelper extends PddaConfigHelper {
     private static final Logger LOG = LoggerFactory.getLogger(PddaDlNotifierHelper.class);
 
-    private static final String CONFIG_DL_NOTIFIER_PROVIDER_URL = "DL_NOTIFIER_PROVIDER_URL";
-    private static final String CONFIG_DL_NOTIFIER_CONNECTION_FACTORY = "DL_NOTIFIER_CONNECTION_FACTORY";
-    private static final String CONFIG_DL_NOTIFIER_DESTINATION = "DL_NOTIFIER_DESTINATION";
-    private static final String CONFIG_DL_NOTIFIER_EXECUTION_TIME = "DL_NOTIFIER_EXECUTION_TIME";
-   
-    
+    private static final String CONFIG_DL_NOTIFIER_EXECUTION_TIME = "DL_NOTIFIER_EXECUTION_TIME";    
     private static final String YES = "Y";
     private static final String STR_DATEFORMAT = "dd/MM/yyyy";
     private static final DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern(STR_DATEFORMAT);
-    private static final DateTimeFormatter DATETIMEFORMAT =
-        DateTimeFormatter.ofPattern("dd/MM/yyyy_HH:mm:ss");
     private static final DateTimeFormatter EXECUTIONTIMEFORMAT =
         DateTimeFormatter.ofPattern(STR_DATEFORMAT + "HH:mm");
 
     private XhbPddaDlNotifierRepository pddaDlNotifierRepository;
     private XhbCourtRepository courtRepository;
-    private Notifier notifier;
 
     public PddaDlNotifierHelper(EntityManager entityManager) {
         super(entityManager);
@@ -96,19 +86,12 @@ public class PddaDlNotifierHelper extends PddaConfigHelper {
     }
 
     public void runDailyListNotifier() {
-        String providerUrl = getConfigValue(CONFIG_DL_NOTIFIER_PROVIDER_URL);
-        String connectionFactoryName = getConfigValue(CONFIG_DL_NOTIFIER_CONNECTION_FACTORY);
-        String destinationName = getConfigValue(CONFIG_DL_NOTIFIER_DESTINATION);
-
-        String date = LocalDateTime.now().format(DATETIMEFORMAT);
-
         // Loop through all the active courts
         List<XhbCourtDao> allCourts = getCourtRepository().findAll();
         if (allCourts != null) {
             for (XhbCourtDao court : allCourts) {
                 if (!YES.equals(court.getObsInd())) {
-                    runDailyListNotifier(providerUrl, connectionFactoryName, destinationName,
-                        court.getCourtId(), date);
+                    runDailyListNotifier(court.getCourtId());
                 }
             }
         }
@@ -117,8 +100,7 @@ public class PddaDlNotifierHelper extends PddaConfigHelper {
     /*
      * Notify the public display for this court that there is a new days list.
      */
-    private void runDailyListNotifier(String providerUrl, String connectionFactoryName,
-        String destinationName, Integer courtId, String date) {
+    private void runDailyListNotifier(Integer courtId) {
         methodName = "runDailyListNotifier(" + courtId + ")";
         LOG.debug(methodName + " called");
         LocalDateTime lastRunDate = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT);
@@ -133,26 +115,10 @@ public class PddaDlNotifierHelper extends PddaConfigHelper {
             DlNotifierStatusEnum runnningEnum = DlNotifierStatusEnum.RUNNING;
             setStatus(dao, runnningEnum.getStatus(), null);
 
-            // Fire off the notifier
+            // Set the status as SUCCESS
             DlNotifierStatusEnum successEnum = DlNotifierStatusEnum.SUCCESS;
             String status = successEnum.getStatus();
-            String errorMessage = null;
-            try {
-                getNotifier().setProviderUrl(providerUrl);
-                getNotifier().setConnectionFactoryName(connectionFactoryName);
-                getNotifier().setDestinationName(destinationName);
-                getNotifier().setCourtId(courtId);
-                getNotifier().setDate(date);
-                getNotifier().run();
-            } catch (Exception ex) {
-                DlNotifierStatusEnum failureEnum = DlNotifierStatusEnum.FAILURE;
-                status = failureEnum.getStatus();
-                errorMessage = ex.getMessage();
-            } finally {
-                // Refetch the record ready for updating
-                dao = findByCourtAndLastRunDate(courtId, lastRunDate);
-                setStatus(dao, status, errorMessage);
-            }
+            setStatus(dao, status, null);
         }
     }
 
@@ -193,53 +159,5 @@ public class PddaDlNotifierHelper extends PddaConfigHelper {
             courtRepository = new XhbCourtRepository(entityManager);
         }
         return courtRepository;
-    }
-
-    private Notifier getNotifier() {
-        if (notifier == null) {
-            notifier = new Notifier();
-        }
-        return notifier;
-    }
-
-    public class Notifier {
-        private String providerUrl;
-        private String connectionFactoryName;
-        private String destinationName;
-        private int courtId;
-        private String date;
-
-        public void run() {
-            
-            NotifierAttributes notifier = new NotifierAttributes();
-            
-            notifier.setProviderUrl(providerUrl);
-            notifier.setConnectionFactoryName(connectionFactoryName);
-            notifier.setDestinationName(destinationName);
-            notifier.setCourtId(courtId);
-            notifier.setDate(date);
-            
-            DailyListNotifier.main(notifier);
-        }
-
-        public void setProviderUrl(String providerUrl) {
-            this.providerUrl = providerUrl;
-        }
-
-        public void setConnectionFactoryName(String connectionFactoryName) {
-            this.connectionFactoryName = connectionFactoryName;
-        }
-
-        public void setDestinationName(String destinationName) {
-            this.destinationName = destinationName;
-        }
-
-        public void setCourtId(int courtId) {
-            this.courtId = courtId;
-        }
-
-        public void setDate(String date) {
-            this.date = date;
-        }
     }
 }
