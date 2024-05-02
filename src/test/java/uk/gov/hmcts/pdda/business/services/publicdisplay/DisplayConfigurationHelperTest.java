7b419425f698918e6f14e696a784b342ee9ed4be
markharriscgi <mark.harris1@hmcts.net>
2024-05-02 13:52:09 +0100

    pdda-350 Stop the sonarqube complaining

--- src/test/java/uk/gov/hmcts/pdda/business/services/publicdisplay/DisplayConfigurationHelperTest.java
+++ src/test/java/uk/gov/hmcts/pdda/business/services/publicdisplay/DisplayConfigurationHelperTest.java
@@ -35,7 +35,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-@SuppressWarnings("static-access")
+@SuppressWarnings({"static-access","TooManyMethods"})
 @ExtendWith(EasyMockExtension.class)
 class DisplayConfigurationHelperTest {
 
