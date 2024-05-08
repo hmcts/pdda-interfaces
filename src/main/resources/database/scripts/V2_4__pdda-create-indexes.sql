efd4c1862cd6e0972635fb02375449ce30f699cd
markharriscgi <mark.harris1@hmcts.net>
2024-05-08 14:16:52 +0100

    pdda-354 Make sure the xhb_config_prop.property_name is unique.

--- src/main/resources/database/scripts/V2_4__pdda-create-indexes.sql
+++ src/main/resources/database/scripts/V2_4__pdda-create-indexes.sql
@@ -2,3 +2,4 @@ SET client_encoding TO 'UTF8';
 
 CREATE INDEX xhb_courtel_list_xml_doc_idx ON xhb_courtel_list (xml_document_id, courtel_list_id);
 CREATE INDEX xhb_document_clob_idx ON xhb_xml_document (xml_document_clob_id, xml_document_id);
+CREATE UNIQUE INDEX xhb_config_prop_idx ON xhb_config_prop (property_name);
