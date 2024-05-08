77e28414e059abf74056664423dfacfdff9f8a75
markharriscgi <mark.harris1@hmcts.net>
2024-05-08 13:30:23 +0100

    pdda-354 Add the indexes for the queries.

--- src/main/resources/database/scripts/V2_4__pdda-create-indexes.sql
+++ src/main/resources/database/scripts/V2_4__pdda-create-indexes.sql
@@ -1,1 +1,4 @@
 SET client_encoding TO 'UTF8';
+
+CREATE INDEX xhb_courtel_list_xml_doc_idx ON xhb_courtel_list (xml_document_id, courtel_list_id);
+CREATE INDEX xhb_document_clob_idx ON xhb_xml_document (xml_document_clob_id, xml_document_id);
