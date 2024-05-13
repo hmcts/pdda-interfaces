6d4fe22b6b12c40c49c59537c4c6dd3256c0d06b
nathantoft <154344032+nathantoft@users.noreply.github.com>
2024-05-13 15:07:54 +0100

    Adding test data for xhb_courtel_list

---
+++ src/main/resources/database/test-data/XHB_COURTEL_LIST_DATA_TABLE.sql
@@ -0,0 +1,6 @@
+INSERT INTO xhb_courtel_list 
+(courtel_list_id,xml_document_id,xml_document_clob_id,blob_id,sent_to_courtel,num_send_attempts,last_attempt_datetime,message_text,last_update_date,creation_date,created_by,last_updated_by,"version") 
+VALUES
+	 (1,491000,421000,1,'N',5,'2018-11-07 00:00:00.000','Filename is invalid :Warned List from 29/10/18 to 02/11/18 DRAFT v2 2018-10-31 10:30:39','2024-05-13 
+13:50:23.640','2024-05-13 13:50:23.640','PDDA','PDDA',1),
+	 (2,491000,421001,1,'N',5,'2018-11-07 00:00:00.000','Date in filename is invalid','2024-05-13 14:43:26.998','2024-05-13 14:43:26.998','PDDA','PDDA',1);
