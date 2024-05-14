ab9e50c2dfb588f16f884cb14c6ef40f237fb5d5
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-05-14 13:07:47 +0100

    Revert "Remove the failing smoke test"
    This reverts commit a1d454a748964307f300706dda0e7d058ad1f256.

---
+++ src/smokeTest/java/uk/gov/hmcts/controller/SetupControllerSmokeTest.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SetupControllerSmokeTest {
+    protected static final String CONTENT_TYPE_VALUE = "application/json";
+
+    @Value("${TEST_URL:http://localhost:8080/DisplaySelectorServlet?}")
+    private String testUrl;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = testUrl;
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Test
+    void smokeTest() {
+        Response response = given()
+            .contentType(ContentType.HTML)
+            .when()
+            .get()
+            .then()
+            .extract().response();
+        
+        Assertions.assertEquals(200, response.statusCode());
+    } 
+}
