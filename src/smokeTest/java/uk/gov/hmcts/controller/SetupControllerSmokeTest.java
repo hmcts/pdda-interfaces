2ec1d7ccc56cf78bf823d728cffceecc8622a74c
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-05-17 15:35:01 +0100

    Revert "Remove the LOG to mirror the other working tests in github"
    This reverts commit 011addd69a595298157f3ee3a3d7423d82d4ea79.

--- src/smokeTest/java/uk/gov/hmcts/controller/SetupControllerSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/controller/SetupControllerSmokeTest.java
@@ -6,6 +6,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,6 +15,8 @@ import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SetupControllerSmokeTest {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(SetupControllerSmokeTest.class);
     
     @Value("${TEST_URL:http://localhost:8080}")
     private String testUrl;
@@ -32,6 +36,7 @@ class SetupControllerSmokeTest {
             .then()
             .extract().response();
         
+        LOG.info("Smoketest.status={}",response.statusCode());
         Assertions.assertEquals(200, response.statusCode());
     } 
 }
