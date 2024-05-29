b22186049bc0605ee5ab22be78842caa6b63b94f
Luke Gittins <luke.gittins@hmcts.net>
2024-05-29 11:10:33 +0100

    Adding the BlobHelper

---
+++ src/main/java/uk/gov/hmcts/pdda/business/services/pdda/BlobHelper.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.pdda.business.services.pdda;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.hmcts.pdda.business.entities.xhbblob.XhbBlobDao;
+import uk.gov.hmcts.pdda.business.entities.xhbblob.XhbBlobRepository;
+import uk.gov.hmcts.pdda.business.services.formatting.FormattingServiceUtils;
+
+import java.util.Optional;
+
+/**
+ * <p>
+ * Title: BlobHelper.
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2024
+ * </p>
+ * <p>
+ * Company: CGI
+ * </p>
+ * 
+ * @author Luke Gittins
+ * @version 1.0
+ */
+public class BlobHelper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BlobHelper.class);
+
+    private final XhbBlobRepository xhbBlobRepository;
+
+    public BlobHelper(XhbBlobRepository xhbBlobRepository) {
+        this.xhbBlobRepository = xhbBlobRepository;
+    }
+
+    public Long createBlob(byte[] blobData) {
+        LOG.debug("createBlob({})", blobData);
+        XhbBlobDao dao = FormattingServiceUtils.createXhbBlobDao(blobData);
+        Optional<XhbBlobDao> savedDao = xhbBlobRepository.update(dao);
+        return savedDao.isPresent() ? savedDao.get().getBlobId() : null;
+    }
+
+    public byte[] getBlobData(Long blobId) {
+        Optional<XhbBlobDao> savedDao = xhbBlobRepository.findById(blobId);
+        return savedDao.isPresent() ? savedDao.get().getBlobData() : null;
+    }
+}
