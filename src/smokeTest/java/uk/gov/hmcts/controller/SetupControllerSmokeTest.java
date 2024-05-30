217be1c1e294687bbf350e74d4625349ca08596c
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-05-30 09:18:02 +0100

    Add more debug to the smoketest

--- src/smokeTest/java/uk/gov/hmcts/controller/SetupControllerSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/controller/SetupControllerSmokeTest.java
@@ -10,13 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
 import static io.restassured.RestAssured.given;
 
+@Configuration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SetupControllerSmokeTest {
     
     private static final Logger LOG = LoggerFactory.getLogger(SetupControllerSmokeTest.class);
+    private static final String DB_HOST = "pdda.db_host";
+    private static final String DB_PORT = "pdda.db_port";
     
     @Value("${TEST_URL:http://localhost:8080}")
     private String testUrl;
@@ -30,16 +34,17 @@ class SetupControllerSmokeTest {
     @Test
     void smokeTest() {
         LOG.info("testUrl={}", testUrl);
+        LOG.info("DB_HOST: {}",DB_HOST);
+        LOG.info("DB_PORT: {}",DB_PORT);
         Response response = given()
-            .contentType(ContentType.JSON)
+            .contentType(ContentType.HTML)
             .when()
             .get()
             .then()
             .extract().response();
         
-        LOG.info("Smoketest.pageContent={}",response.asString());
-        Assertions.assertTrue(response.asString().startsWith("Display"));
         LOG.info("Smoketest.status={}",response.statusCode());
+        LOG.info("Smoketest.pageContent={}",response.asString());
         Assertions.assertEquals(200, response.statusCode());
     } 
 }
