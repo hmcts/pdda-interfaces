d0436c8a511ad793627355f2ad26504bc1e41e01
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-08-13 10:13:37 +0100

    pdda-393 Apply the environment variables rename to pdda2 code.

--- src/main/resources/database/data/V3_3_1__XHB_CONFIG_PROP_DATA_TABLE.sql
+++ src/main/resources/database/data/V3_3_1__XHB_CONFIG_PROP_DATA_TABLE.sql
@@ -2,14 +2,14 @@ CREATE UNIQUE INDEX IF NOT EXISTS xhb_config_prop_idx ON xhb_config_prop (proper
 insert into xhb_config_prop
 (config_prop_id, property_name, property_value)
 values
-(6, 'scheduledtasks.pdda', 'courtellisttask'),
-(19, 'COURTEL_LIST_AMOUNT', '5'),
-(20, 'COURTEL_MAX_RETRY', '5'),
-(21, 'MESSAGE_LOOKUP_DELAY', '60'),
-(22, 'CPPX_SchemaWL', 'WarnedList-v1-0.xsd'),
-(23, 'CPPX_SchemaFL', 'FirmList-v1-0.xsd'),
-(24, 'CPPX_SchemaWP', 'CPPX_InternetWebPage-v1-0.xsd')
+(nextval('xhb_config_prop_seq'), 'scheduledtasks.pdda', 'transformtask,cppformattingtask,cppstagingtask,pddabaistask,dlnotifiertask,lighthousetask, courtellisttask'),
+(nextval('xhb_config_prop_seq'), 'COURTEL_LIST_AMOUNT', '5'),
+(nextval('xhb_config_prop_seq'), 'COURTEL_MAX_RETRY', '5'),
+(nextval('xhb_config_prop_seq'), 'MESSAGE_LOOKUP_DELAY', '60'),
+(nextval('xhb_config_prop_seq'), 'CPPX_SchemaWL', 'WarnedList-v1-0.xsd'),
+(nextval('xhb_config_prop_seq'), 'CPPX_SchemaFL', 'FirmList-v1-0.xsd'),
+(nextval('xhb_config_prop_seq'), 'CPPX_SchemaWP', 'CPPX_InternetWebPage-v1-0.xsd')
 on conflict (property_name)
 do update
-set property_value = xhb_config_prop.property_value||','||EXCLUDED.property_value
+set property_value = EXCLUDED.property_value
 where POSITION(EXCLUDED.property_value in xhb_config_prop.property_value) = 0;
