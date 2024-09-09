a21fd498d13fcf106d9fecf5639235ad2b0be5d2
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-09-09 11:50:56 +0100

    pdda-407 Add the tests for helper.

--- src/main/java/uk/gov/hmcts/pdda/business/services/pdda/ClearDownHelper.java
+++ src/main/java/uk/gov/hmcts/pdda/business/services/pdda/ClearDownHelper.java
@@ -7,8 +7,11 @@ import uk.gov.hmcts.pdda.business.entities.xhbconfigprop.XhbConfigPropDao;
 import uk.gov.hmcts.pdda.business.entities.xhbconfigprop.XhbConfigPropRepository;
 import uk.gov.hmcts.pdda.business.entities.xhbcrlivedisplay.XhbCrLiveDisplayDao;
 import uk.gov.hmcts.pdda.business.entities.xhbcrlivedisplay.XhbCrLiveDisplayRepository;
+import uk.gov.hmcts.pdda.web.publicdisplay.rendering.compiled.DateUtils;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 /**
@@ -31,14 +34,24 @@ import java.util.List;
 public class ClearDownHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(ClearDownHelper.class);
-    private static final String RESET_DISPLAY_IWP_TIME = "RESET_DISPLAY_IWP_TIME";
+    public static final DateTimeFormatter DATETIME_FORMAT =
+        DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+    public static final String RESET_DISPLAY_IWP_TIME = "RESET_DISPLAY_IWP_TIME";
 
     private final EntityManager entityManager;
-    private XhbCrLiveDisplayRepository crLiveDisplayRepository;
-    private XhbConfigPropRepository configPropRepository;
+    private XhbCrLiveDisplayRepository xhbCrLiveDisplayRepository;
+    private XhbConfigPropRepository xhbConfigPropRepository;
 
     public ClearDownHelper(EntityManager entityManager) {
         this.entityManager = entityManager;
+    }
+
+    // JUnit Constructor
+    public ClearDownHelper(XhbCrLiveDisplayRepository xhbCrLiveDisplayRepository,
+        XhbConfigPropRepository xhbConfigPropRepository) {
+        this(null);
+        this.xhbCrLiveDisplayRepository = xhbCrLiveDisplayRepository;
+        this.xhbConfigPropRepository = xhbConfigPropRepository;
     }
 
     /**
@@ -64,7 +77,12 @@ public class ClearDownHelper {
      */
     public LocalDateTime getClearDownTime(String propertyValue) {
         LOG.debug("getclearDownTime({})", propertyValue);
-        return LocalDateTime.parse(propertyValue);
+        String todaysDate = DateUtils.getDate(LocalDateTime.now());
+        try {
+            return LocalDateTime.parse(todaysDate + propertyValue, DATETIME_FORMAT);
+        } catch (DateTimeParseException ex) {
+            return null;
+        }
     }
 
     /**
@@ -73,7 +91,7 @@ public class ClearDownHelper {
     public void resetLiveDisplays() {
         LOG.debug("resetLiveDisplays()");
         List<XhbCrLiveDisplayDao> liveDisplays =
-            getCrLiveDisplayRepository().findLiveDisplaysWhereStatusNotNull();
+            getXhbCrLiveDisplayRepository().findLiveDisplaysWhereStatusNotNull();
         if (!liveDisplays.isEmpty()) {
             LOG.debug("Found {} live displays to be reset", liveDisplays.size());
             for (XhbCrLiveDisplayDao liveDisplay : liveDisplays) {
@@ -87,23 +105,23 @@ public class ClearDownHelper {
             LOG.debug("Setting display {} status {} to null", liveDisplay.getCrLiveDisplayId(),
                 liveDisplay.getStatus());
             liveDisplay.setStatus(null);
-            getCrLiveDisplayRepository().update(liveDisplay);
+            getXhbCrLiveDisplayRepository().update(liveDisplay);
         } catch (Exception ex) {
             LOG.error("Error: {}", ex.getMessage());
         }
     }
 
-    private XhbCrLiveDisplayRepository getCrLiveDisplayRepository() {
-        if (crLiveDisplayRepository == null) {
-            crLiveDisplayRepository = new XhbCrLiveDisplayRepository(entityManager);
+    private XhbCrLiveDisplayRepository getXhbCrLiveDisplayRepository() {
+        if (xhbCrLiveDisplayRepository == null) {
+            xhbCrLiveDisplayRepository = new XhbCrLiveDisplayRepository(entityManager);
         }
-        return crLiveDisplayRepository;
+        return xhbCrLiveDisplayRepository;
     }
 
     private XhbConfigPropRepository getXhbConfigPropRepository() {
-        if (configPropRepository == null) {
-            configPropRepository = new XhbConfigPropRepository(entityManager);
+        if (xhbConfigPropRepository == null) {
+            xhbConfigPropRepository = new XhbConfigPropRepository(entityManager);
         }
-        return configPropRepository;
+        return xhbConfigPropRepository;
     }
 }
