f29e9b1dd5e8aa07f0a05760cda0c56629075bc1
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2024-09-16 15:09:21 +0100

    pdda-409 Fix the EventType

--- src/main/java/uk/gov/courtservice/xhibit/common/publicdisplay/events/types/EventType.java
+++ src/main/java/uk/gov/courtservice/xhibit/common/publicdisplay/events/types/EventType.java
@@ -120,7 +120,7 @@ public final class EventType {
      */
     @Override
     public boolean equals(Object anObject) {
-        return anObject instanceof EventType;
+        return anObject instanceof EventType && value.equals(((EventType) anObject).value);
     }
 
     /**
