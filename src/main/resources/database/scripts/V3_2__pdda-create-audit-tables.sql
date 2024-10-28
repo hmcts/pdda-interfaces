3fc988a8bae11d27e1210ff19fc83c79ca84bdb8
Luke Gittins <luke.gittins@hmcts.net>
2024-10-28 12:08:41 +0000

    PDDA-421 Moving XHB_CATH_DOCUMENT_LINK table creation scripts into V4 to avoid staging conflict

--- src/main/resources/database/scripts/V3_2__pdda-create-audit-tables.sql
+++ src/main/resources/database/scripts/V3_2__pdda-create-audit-tables.sql
@@ -18,17 +18,3 @@ CREATE TABLE aud_courtel_list (
 	insert_event varchar(1) NOT NULL
 );
 ALTER TABLE aud_courtel_list ADD CONSTRAINT aud_courtel_list_pk PRIMARY KEY (courtel_list_id, last_update_date);
-
-DROP TABLE IF EXISTS aud_cath_document_link CASCADE;
-CREATE TABLE aud_cath_document_link (
-	cath_document_link_id integer NOT NULL,
-	orig_courtel_list_doc_id integer,
-	cath_xml_id integer,
-	cath_json_id integer,
-	created_by varchar(30) NOT NULL,
-	creation_date timestamp NOT NULL,
-	last_updated_by varchar(30) NOT NULL,
-	last_update_date timestamp NOT NULL,
-	version integer NOT NULL
-);
-ALTER TABLE aud_cath_document_link ADD CONSTRAINT aud_cath_document_link_pk PRIMARY KEY (cath_document_link_id);
