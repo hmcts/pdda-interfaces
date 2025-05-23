d14e8572d6c78d6588332ac45ad3443e905dde39
mark.e.harris@cgi.com <mark.e.harris@cgi.com>
2025-05-23 15:20:14 +0100

    Attempt 2 to fix the autowired issue.

--- src/main/java/uk/gov/hmcts/controller/SetupController.java
+++ src/main/java/uk/gov/hmcts/controller/SetupController.java
@@ -2,32 +2,19 @@ package uk.gov.hmcts.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import uk.gov.hmcts.pdda.web.publicdisplay.initialization.servlet.InitializationService;
 
 @RestController
 @Service
 @RequestMapping("/setup")
-@SuppressWarnings("PMD.SingularField")
 public class SetupController {
-    
-    @Autowired
-    private Environment env;
 
     /** Logger. */
     private static Logger log = LoggerFactory.getLogger(SetupController.class);
-    
-    public SetupController() {
-        super();
-        log.info("Env = {}", env);
-        InitializationService.getInstance().setEnvironment(env);
-    }
 
     @GetMapping("/DisplaySelectorServlet")
     public ModelAndView displaySelectorServlet(ModelAndView model) {
