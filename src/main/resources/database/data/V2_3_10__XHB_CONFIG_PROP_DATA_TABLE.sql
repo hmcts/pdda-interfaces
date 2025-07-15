607ae3082b6eec5a07a26ef4cc4ce126539ff92f
Luke Gittins <luke.gittins@hmcts.net>
2025-07-15 17:22:37 +0100

    PDDA-532 Adding the environment url to the XHB_CONFIG_PROP with a flyway script

---
+++ src/main/resources/database/data/V2_3_10__XHB_CONFIG_PROP_DATA_TABLE.sql
@@ -0,0 +1,5 @@
+Insert into XHB_CONFIG_PROP
+   (CONFIG_PROP_ID, PROPERTY_NAME, PROPERTY_VALUE)
+ Values
+   (91, 'PDDA_PDM_ENVIRONMENT_URL', 'https://pdda-public-display-manager.demo.platform.hmcts.net');
+COMMIT;
