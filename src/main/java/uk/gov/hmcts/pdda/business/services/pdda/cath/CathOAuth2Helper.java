8ff0feb84199ef4e4d4b7417e90def01b2a39bd8
Luke Gittins <luke.gittins@hmcts.net>
2025-08-21 13:39:05 +0100

    PDDA-465 Adding logging to make sure the cath credentials are being used

--- src/main/java/uk/gov/hmcts/pdda/business/services/pdda/cath/CathOAuth2Helper.java
+++ src/main/java/uk/gov/hmcts/pdda/business/services/pdda/cath/CathOAuth2Helper.java
@@ -35,6 +35,7 @@ public class CathOAuth2Helper extends OAuth2Helper {
 
     @Override
     protected String getTokenUrl() {
+        LOG.debug("Getting CaTH Token URL {}", CATH_AZURE_TOKEN_URL);
         String tokenUrl = env.getProperty(CATH_AZURE_TOKEN_URL);
         if (tokenUrl == null) {
             LOG.error("Token URL property '{}' not found in environment.", CATH_AZURE_TOKEN_URL);
@@ -45,6 +46,7 @@ public class CathOAuth2Helper extends OAuth2Helper {
 
     @Override
     protected String getClientId() {
+        LOG.debug("Getting CaTH Client ID {}", CATH_AZURE_CLIENT_ID);
         String clientId = env.getProperty(CATH_AZURE_CLIENT_ID);
         if (clientId == null) {
             LOG.error("Client ID property '{}' not found in environment.", CATH_AZURE_CLIENT_ID);
@@ -55,6 +57,7 @@ public class CathOAuth2Helper extends OAuth2Helper {
 
     @Override
     protected String getClientSecret() {
+        LOG.debug("Getting CaTH Client Secret {}", CATH_AZURE_CLIENT_SECRET);
         String clientSecret = env.getProperty(CATH_AZURE_CLIENT_SECRET);
         if (clientSecret == null) {
             LOG.error("Client Secret property '{}' not found in environment.", CATH_AZURE_CLIENT_SECRET);
