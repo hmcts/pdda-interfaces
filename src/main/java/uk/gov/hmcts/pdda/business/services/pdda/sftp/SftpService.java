921315debc07318ee2824d16b81617170aa9f25c
Luke Gittins <luke.gittins@hmcts.net>
2025-10-22 09:45:28 +0100

    Adding null checks around logging for the hearingProgressEvent

--- src/main/java/uk/gov/hmcts/pdda/business/services/pdda/sftp/SftpService.java
+++ src/main/java/uk/gov/hmcts/pdda/business/services/pdda/sftp/SftpService.java
@@ -47,7 +47,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @SuppressWarnings({"PMD.CyclomaticComplexity", "PMD.ExcessiveParameterList", 
-    "PMD.CouplingBetweenObjects", "PMD.ExcessiveImports"})
+    "PMD.CouplingBetweenObjects", "PMD.ExcessiveImports", "PMD.CognitiveComplexity", "PMD.NPathComplexity"})
 public class SftpService extends XhibitPddaHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(SftpService.class);
@@ -383,17 +383,23 @@ public class SftpService extends XhibitPddaHelper {
         // 1) Get the courtId from the courtName from the event
         List<XhbCourtDao> xhbCourtDao = getCourtRepository()
             .findByCourtNameValueSafe(event.getCourtName());
-        LOG.debug("Court found with ID: {}", xhbCourtDao.get(0).getCourtId());
+        if (!xhbCourtDao.isEmpty()) {
+            LOG.debug("Court found with ID: {}", xhbCourtDao.get(0).getCourtId());
+        }
         
         // 2) Get the courtSiteId from the courtId
         XhbCourtSiteDao xhbCourtSiteDao = getCourtSiteRepository()
             .findByCourtIdSafe(xhbCourtDao.get(0).getCourtId()).get(0);
-        LOG.debug("Court site found with ID: {}", xhbCourtSiteDao.getCourtId());
+        if (xhbCourtSiteDao != null) {
+            LOG.debug("Court site found with ID: {}", xhbCourtSiteDao.getCourtId());
+        }
         
         // 3) Get the caseType and caseNumber from the event
         String caseType = event.getCaseType();
         Integer caseNumber = event.getCaseNumber();
-        LOG.debug("Case number from event: {}{}", caseType, caseNumber);
+        if (caseType != null && caseNumber != null) {
+            LOG.debug("Case number from event: {}{}", caseType, caseNumber);
+        }
         
         // 4) Get caseId using the case number from the event
         Optional<XhbCaseDao> xhbCaseDao = getCaseRepository()
@@ -404,21 +410,30 @@ public class SftpService extends XhibitPddaHelper {
             // 5) Get the hearingId using the caseId
             List<XhbHearingDao> xhbHearingDao = getHearingRepository()
                 .findByCaseIdSafe(xhbCaseDao.get().getCaseId());
-            LOG.debug("Hearing found with ID: {}", xhbHearingDao.get(0).getHearingId());
+            if (!xhbHearingDao.isEmpty()) {
+                LOG.debug("Hearing found with ID: {}", xhbHearingDao.get(0).getHearingId());
+            }
             
             // 6) Get the courtRoomId from the courtSiteId and courtRoomName
             String courtRoomName = event.getCourtRoomName();
-            LOG.debug("Court room name from event: {}", courtRoomName);
+            if (courtRoomName != null) {
+                LOG.debug("Court room name from event: {}", courtRoomName);
+            }
+            
             XhbCourtRoomDao xhbCourtRoomDao = getCourtRoomRepository()
                 .findByCourtSiteIdAndCourtRoomNameSafe(xhbCourtSiteDao.getCourtSiteId(), courtRoomName).get(0);
-            LOG.debug("Court room found with ID: {}", xhbCourtRoomDao.getCourtRoomId());
+            if (xhbCourtRoomDao != null) {
+                LOG.debug("Court room found with ID: {}", xhbCourtRoomDao.getCourtRoomId());
+            }
             
             // 7) Get the SittingId using the courtRoomId and courtSiteId
             List<XhbSittingDao> xhbSittingDaos = getSittingRepository()
                 .findByCourtRoomIdAndCourtSiteIdWithTodaysSittingDateSafe(xhbCourtRoomDao.getCourtRoomId(),
                     xhbCourtSiteDao.getCourtSiteId(), LocalDate.now().atStartOfDay());
-            LOG.debug("No. of Sittings found using courtRoomId: {} and courtSiteId: {} is {}",
-                xhbCourtRoomDao.getCourtRoomId(), xhbCourtSiteDao.getCourtSiteId(), xhbSittingDaos.size());
+            if (!xhbSittingDaos.isEmpty()) {
+                LOG.debug("No. of Sittings found using courtRoomId: {} and courtSiteId: {} is: {}",
+                    xhbCourtRoomDao.getCourtRoomId(), xhbCourtSiteDao.getCourtSiteId(), xhbSittingDaos.size());
+            }
             
             // 8) Loop through the SittingId's to get a match with the hearingId for the xhb_scheduled_hearing record
             for (XhbSittingDao sittingDao : xhbSittingDaos) {
