5ec82ac50460303ca67645619a0002a900526d56
Luke Gittins <luke.gittins@hmcts.net>
2026-02-20 13:35:54 +0000

    Forcing WithFixedDate and WithoutFixedDate to be arrays when converting to json

--- src/main/java/uk/gov/hmcts/pdda/business/services/pdda/cath/CathUtils.java
+++ src/main/java/uk/gov/hmcts/pdda/business/services/pdda/cath/CathUtils.java
@@ -73,7 +73,8 @@ public final class CathUtils {
     );
     // Keys that should always be represented as JSON arrays
     private static final Set<String> FORCE_ARRAY_KEYS = Set.of(
-            "Counsel", "Solicitor", "CitizenNameForename", "ReserveList", "Hearing", "Fixture", "Location"
+            "Counsel", "Solicitor", "CitizenNameForename", "ReserveList", "Hearing", "Fixture", "Location",
+            "WithoutFixedDate", "WithFixedDate"
     );
     // Keys that should always be represented as JSON strings
     private static final Set<String> FORCE_STRING_KEYS = Set.of(
